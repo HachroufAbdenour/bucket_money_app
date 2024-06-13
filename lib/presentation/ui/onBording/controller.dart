4aacab6f1f6b2aeb66d6b0872c56f4f6901fdf9e
ABDENOUR HACHROUF <abdouopt5@gmail.com>
2024-06-13 19:15:41 +0100

    fixed

--- lib/presentation/ui/onBording/controller.dart
+++ lib/presentation/ui/onBording/controller.dart
@@ -5,6 +5,10 @@ import 'package:image_picker/image_picker.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:money_app/data/local_storage/local_storage.dart';
 import 'package:money_app/presentation/ui/transactions/transactions_screen.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+
+
 
 class ViewController extends GetxController {
   final fullnameController = TextEditingController();
@@ -18,13 +22,51 @@ class ViewController extends GetxController {
     _storage.writeIfNull('fullName', '');
   }
 
-  Future<void> pickImage() async {
+ Future<void> pickImage() async {
+  final bool permissionGranted = await _requestPermission(Permission.storage);
+  if (permissionGranted) {
+    await _getDeviceInfo();
     final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
       image.value = File(pickedFile.path);
       _storage.write('image', pickedFile.path); // Save image path to storage
     }
+  } else {
+    Get.snackbar("Permission Denied", "Storage permission is required to pick an image.");
   }
+}
+
+//  Future<void> pickImage() async {
+//     if (await _requestPermission(Permission.storage)) {
+//     final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+//     if (pickedFile != null) {
+//       image.value = File(pickedFile.path);
+//       _storage.write('image', pickedFile.path); // Save image path to storage
+//     }
+//     } else {
+//       Get.snackbar("Permission Denied", "Storage permission is required to pick an image.");
+//     }
+//   }
+
+Future<bool> _requestPermission(Permission permission) async {
+  final PermissionStatus status = await permission.request();
+  return status.isGranted;
+}
+
+
+Future<void> _getDeviceInfo() async {
+  final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  if (Platform.isAndroid) {
+    final AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    // Use androidInfo for Android device information
+  } else if (Platform.isIOS) {
+    final IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+    // Use iosInfo for iOS device information
+  }
+}
+
+
+
 
   void saveFullName() {
     if (fullnameController.text.isNotEmpty) {
@@ -32,7 +74,10 @@ class ViewController extends GetxController {
     }
   }
 
-  void submit() {
+  void submit() async {
+  final bool permissionGranted = await _requestPermission(Permission.storage);
+  if (permissionGranted) {
+    await _getDeviceInfo();
     if (fullnameController.text.isNotEmpty && image.value != null) {
       saveFullName();
       LocalStorage().setIsFirstTime(false);
@@ -40,5 +85,9 @@ class ViewController extends GetxController {
     } else {
       Get.snackbar("Error", "Please enter all information", backgroundColor: Colors.red, colorText: Colors.white);
     }
+  } else {
+    Get.snackbar("Permission Denied", "Storage permission is required to proceed.");
   }
+}
+
 }
--- lib/presentation/ui/splash/splash_controller.dart
+++ lib/presentation/ui/splash/splash_controller.dart
@@ -1,18 +1,31 @@
 import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:money_app/data/local_storage/local_storage.dart';
 import 'package:money_app/presentation/router/routes.dart';
-import 'package:money_app/shared/utils/utils.dart';
 
 class SplashController extends GetxController {
+  final LocalStorage localStorage = Get.put(LocalStorage());
+
   @override
   void onInit() {
     super.onInit();
-    start();
+    _initializeApp();
   }
 
-  void start() async {
-    await delayedFunc(milliseconds: 3590);
-    goTransactions();
+  Future<void> _initializeApp() async {
+    try {
+      await _simulateLoading();  // Simulate some loading process
+      _navigateBasedOnUserStatus();
+    } catch (e) {
+      print('Error initializing app: $e');
+    }
   }
 
-  void goTransactions() => Get.toNamed(AppRoutes.onBording);
-                     }
+  Future<void> _simulateLoading() async => await Future.delayed(Duration(milliseconds: 3500));
+
+  void _navigateBasedOnUserStatus()async {
+    String route = await localStorage.getIsFirstTime() ? AppRoutes.onBording : AppRoutes.transactions;
+    Get.offAllNamed(route);
+  }
+}
